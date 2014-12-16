1e241d9510e8cd3478f7029a6bad73209c7bce00
Corey Abshire <corey.abshire@gmail.com>
2014-12-16 00:30:39 -0500

    Convert regression to sklearn compatible module.

--- regression.py
+++ regression.py
@@ -11,31 +11,41 @@ import logging
 import simplekml
 import numpy as np
 import pandas as pd
-from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
 
 import sgdb
 import geocode
 import common
 
-X_NAMES = [
-    'ptol_lat',
-    'ptol_lon']
+XCOLS = ['ptol_lat','ptol_lon']
+YCOLS = ['modern_lat','modern_lon']
+NF = range(len(YCOLS))
 
-def derive_unknown_modern_coords(known, unknown):
-    trainx = known.loc[:, X_NAMES]
-    unknownx = unknown.loc[:, X_NAMES]
-    lonreg = linear_model.LinearRegression()
-    latreg = linear_model.LinearRegression()
-    lonreg.fit(trainx, known.modern_lon)
-    latreg.fit(trainx, known.modern_lat)
-    unknown.is_copy = False # prevent warning below that it's a copy of places
-    unknown.loc[:,'modern_lon'] = lonreg.predict(unknownx)
-    unknown.loc[:,'modern_lat'] = latreg.predict(unknownx)
+class Regression(object):
+
+    def __init__(self):
+        self.m = [LinearRegression() for i in NF]
+
+    def fit(self, X, y):
+        for i in NF:
+            self.m[i].fit(X, y.ix[:,i])
+        
+    def predict(self, X):
+        y = np.zeros((len(X),2))
+        for i in NF:
+            y[:,i] = self.m[i].predict(X)
+        return y
 
 def main(filename):
     places = common.read_places()
     known, unknown = common.split_places(places)
-    derive_unknown_modern_coords(known, unknown)
+    knownX = known.loc[:, XCOLS]
+    knownY = known.loc[:, YCOLS]
+    model = Regression()
+    model.fit(knownX, knownY)
+    unknownX = unknown.loc[:, XCOLS]
+    unknownY = model.predict(unknownX)
+    unknown.loc[:,YCOLS] = unknownY
     common.write_kml_file(filename, None, known, unknown)
     common.write_csv_file(filename[0:-4]+'.csv', known, unknown)
 
